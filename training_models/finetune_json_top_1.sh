25f0d2231ef763cc7be370c280781d235c91a4ab
Antonio Scaiella <37184273+piperino11@users.noreply.github.com>
2025-05-19 11:48:03 +0200

    Add files via upload

---
+++ training_models/finetune_json_top_1.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+GPUS_PER_NODE=2
+NNODES=1
+NODE_RANK=0
+MASTER_ADDR=localhost
+MASTER_PORT=6002
+
+MODEL="openbmb/MiniCPM-V-2_6" 
+DATA="./dataset_silver_split/json_top_1_images_train.json"
+EVAL_DATA="./dataset_silver_split/json_top_1_images_dev.json"
+LLM_TYPE="qwen2" 
+
+MODEL_MAX_Length=4096   
+
+DISTRIBUTED_ARGS="
+    --nproc_per_node $GPUS_PER_NODE \
+    --nnodes $NNODES \
+    --node_rank $NODE_RANK \
+    --master_addr $MASTER_ADDR \
+    --master_port $MASTER_PORT
+"
+torchrun $DISTRIBUTED_ARGS ./MiniCPM-V/finetune/finetune.py  \
+    --model_name_or_path $MODEL \
+    --llm_type $LLM_TYPE \
+    --data_path $DATA \
+    --eval_data_path $EVAL_DATA \
+    --remove_unused_columns false \
+    --label_names "labels" \
+    --prediction_loss_only false \
+    --bf16 true  \
+    --bf16_full_eval true \
+    --fp16 false \
+    --fp16_full_eval false \
+    --do_train \
+    --do_eval \
+    --tune_vision true \
+    --tune_llm true \
+    --use_lora false \
+    --lora_target_modules "llm\..*layers\.\d+\.self_attn\.(q_proj|k_proj|v_proj|o_proj)" \
+    --model_max_length $MODEL_MAX_Length \
+    --max_slice_nums 9 \
+    --num_train_epochs 3 \
+    --eval_steps 508 \
+    --output_dir models/json_top_1 \
+    --logging_dir models/json_top_1_log \
+    --logging_strategy "steps" \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "steps" \
+    --save_strategy "steps" \
+    --save_steps 508 \
+    --save_total_limit 6 \
+    --learning_rate 1e-6 \
+    --weight_decay 0.1 \
+    --adam_beta2 0.95 \
+    --warmup_ratio 0.01 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --gradient_checkpointing true \
+    --deepspeed ./MiniCPM-V/finetune/ds_config_zero3.json \
+    --report_to "tensorboard" # wandb
